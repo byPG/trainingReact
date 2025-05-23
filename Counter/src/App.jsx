b33fa2acd6b646b5ea1be1fb6baacf1b4b47d367
byPG <67753466+byPG@users.noreply.github.com>
2025-05-23 11:00:08 +0200

    set up mini exercise

---
+++ Counter/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Counter</h1>
+        <p>use state exercise</p>
+      </header>
+      <main>
+        <button>Kliknij!</button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
