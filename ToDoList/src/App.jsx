f2fe6d50728c445dab09b6870428245436531e70
byPG <67753466+byPG@users.noreply.github.com>
2025-05-30 23:23:58 +0200

    styling

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -12,29 +12,29 @@ function App() {
 
   return (
     <div className="content">
-      <header>
-        {/* <img src="" alt="" /> */}
-        <h2>To Do List</h2>
-      </header>
-
-      <div>
-        <input
-          type="text"
-          placeholder="Write here your new task..."
-          onChange={(e) => setNewTask(e.target.value)}
-        />
-        <button onClick={addTask}>Add task!</button>
-      </div>
-
-      <div>
-        <ul>
-          {tasksList.map((task, index) => (
-            <li key={index}>
-              <input type="checkbox" />
-              {task}
-            </li>
-          ))}
-        </ul>
+      {/* <img src="" alt="" /> */}
+      <div className="mainContent">
+        <header>
+          <h2>Todo App</h2>
+        </header>
+        <div>
+          <input
+            type="text"
+            placeholder="Write here your new task..."
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+          <button onClick={addTask}>+</button>
+        </div>
+        <div>
+          <ul>
+            {tasksList.map((task, index) => (
+              <li key={index}>
+                <input type="checkbox" />
+                {task}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
